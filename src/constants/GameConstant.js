e61f9847aee84a9d17ae166701b5f503f30e97d9
mayeng20 <mariellelagare3@gmail.com>
2025-10-17 10:42:58 +0800

    Change Grid size

--- src/constants/GameConstant.js
+++ src/constants/GameConstant.js
@@ -2,7 +2,7 @@ import { Dimensions } from 'react-native';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
-export const GRID_SIZE = 15;
+export const GRID_SIZE = 20;
 export const GAME_WIDTH = 300;
 export const GAME_HEIGHT = 300;
 export const INITIAL_SNAKE = [{ x: 5, y: 5 }, { x: 4, y: 5 }, { x: 3, y: 5 }];
